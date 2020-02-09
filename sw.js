340bd656c6cca38ee2f08c16fe33c9bb149dc46a
museband <2253783225@qq.com>
2020-02-09 12:15:40 +0800

    Site updated: 2020-02-09 12:15:38

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2018/01/16/Christmas-Party-x-乐队回顾/');
+  precacheUrls.push('/2017/11/12/2017万圣节快闪回顾/');
+
+  precacheUrls.push('/2017/09/05/2017届-第一次全员大会/');
+
+  precacheUrls.push('/1000/01/01/readme-md/');
 
   precacheUrls.push('/2017/08/15/Muse-HFI-你的模样/');
 
-  precacheUrls.push('/2019/08/18/Resonance-音乐节/');
-
-  precacheUrls.push('/2017/12/31/Voice-of-HFI-x-我与Muse的故事/');
-
-  precacheUrls.push('/1000/01/01/readme-md/');
+  precacheUrls.push('/2019/10/10/「10-10-2019-yi-st」十月十三号华师附中国际部MUSE社慈善音乐PA！/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
